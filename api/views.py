051a63727035676894e6b778b7ab1848a04c55a8
Bharath Raj S <bharathraj0643@gmail.com>
2025-05-17 11:41:25 +0530

    storing data from post using serializers

--- api/views.py
+++ api/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from .serializers import StudentSerializer
+from students.models import Student
 
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
+
+@api_view(['GET','POST'])
 def studentsView(request):
-    students = {
-        'id':1,
-        'name': 'Ratan',
-        'class' : 'DRF'
-    }
-    return JsonResponse(students)
+    if request.method == 'GET':
+        students = Student.objects.all()
+        serializer = StudentSerializer(students, many=True)
+        return Response(serializer.data , status=status.HTTP_200_OK)
+    elif request.method == 'POST':
+        serializer = StudentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status = status.HTTP_201_CREATED)
+        print(serializer.errors)
+        return Response(serializer.errors , status=status.HTTP_400_BAD_REQUEST)
