b6d52876252b205bc5f8cf8d8827f261c2f05158
Bharath Raj S <bharathraj0643@gmail.com>
2025-05-19 10:56:56 +0530

    modified:   .gitignore 	new file:   db.sqlite3 class based view , get and post methods of employees

--- api/views.py
+++ api/views.py
@@ -7,6 +7,10 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
 
+from employees.models import Employee
+from rest_framework.views import APIView
+from .serializers import EmployeeSerializer
+
 
 @api_view(["GET", "POST"])
 def studentsView(request):
@@ -43,3 +47,18 @@ def studentDetailView(request, pk):
     elif request.method == "DELETE":
         student.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+    
+
+class Employees(APIView):
+    def get(self, request):
+        employees = Employee.objects.all()
+        serializer = EmployeeSerializer(employees,many=True)
+        return Response(serializer.data , status=status.HTTP_200_OK)
+    
+    def post(self , request):
+        serializer = EmployeeSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data , status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
