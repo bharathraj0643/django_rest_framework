86f79b7aad5a6d8fa86907916835cc545861b3f7
Bharath Raj S <bharathraj0643@gmail.com>
2025-05-19 12:57:25 +0530

    class based view , get and post , put , delete methods of employee detail 	modified:   api/urls.py 	modified:   api/views.py

--- api/views.py
+++ api/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from django.http import Http404, JsonResponse
 from .serializers import StudentSerializer
 from students.models import Student
 
@@ -62,3 +62,27 @@ class Employees(APIView):
             return Response(serializer.data , status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+class EmployeeDetail(APIView):
+    def get_object(self , pk):
+        try:
+            return Employee.objects.get(pk=pk)
+        except Employee.DoesNotExist:
+            raise Http404
+    
+    def get(self,request,pk):
+        employee=self.get_object(pk)
+        serializer = EmployeeSerializer(employee)
+        return Response(serializer.data , status=status.HTTP_200_OK)
+    
+    def put(self,request,pk):
+        employee=self.get_object(pk)
+        serializer = EmployeeSerializer(employee,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_200_OK)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self , request , pk):
+        employee=self.get_object(pk)
+        employee.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
